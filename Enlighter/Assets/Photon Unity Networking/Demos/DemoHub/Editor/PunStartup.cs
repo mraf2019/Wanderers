d077111abc30de0481311cd67c477935db859be6
11111 <77957065+whynotsignin@users.noreply.github.com>
2023-07-26 20:53:11 +0800

    Revert "Revert "。""
    This reverts commit c9940de2540aded6a8ee84404360908ea2cb4865.

--- Enlighter/Assets/Photon Unity Networking/Demos/DemoHub/Editor/PunStartup.cs
+++ Enlighter/Assets/Photon Unity Networking/Demos/DemoHub/Editor/PunStartup.cs
@@ -1,134 +1,166 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PunStartup.cs" company="Exit Games GmbH">
+//   Part of: Photon Unity Demos
+// </copyright>
+// <summary>
+//   Used to setup the demo build settings and load the first demo scene (if imported into a new empty project).
+// </summary>
+// <author>developer@exitgames.com</author>
+// --------------------------------------------------------------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using System.IO;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEditor;
+
+using Photon.Pun;
+using Photon.Realtime;
 using ExitGames.Client.Photon;
+using UnityEngine.SceneManagement;
 
-
-[InitializeOnLoad]
-public class PunStartup : MonoBehaviour
-{ 
-
-    static PunStartup()
+namespace Photon.Pun.Demo.Hub
+{
+    [InitializeOnLoad]
+    public class PunStartup : MonoBehaviour
     {
-        bool doneBefore = EditorPrefs.GetBool("PunDemosOpenedBefore");
-        if (!doneBefore)
-        {
-            EditorApplication.update += OnUpdate;
-        }
-    }
 
-    static void OnUpdate()
-    {
-        if (EditorApplication.isUpdating)
+        static PunStartup()
         {
-            return;
+            bool doneBefore = EditorPrefs.GetBool("PunDemosOpenedBefore");
+            if (!doneBefore)
+            {
+                EditorApplication.update += OnUpdate;
+            }
         }
 
-        bool doneBefore = EditorPrefs.GetBool("PunDemosOpenedBefore");
-        if (doneBefore)
+        static void OnUpdate()
         {
-            EditorApplication.update -= OnUpdate;
-            return;
-        }
+            if (EditorApplication.isUpdating || Application.isPlaying)
+            {
+                return;
+            }
 
-        if (string.IsNullOrEmpty(SceneManagerHelper.EditorActiveSceneName) && EditorBuildSettings.scenes.Length == 0)
-        {
-            LoadPunDemoHub();
-            SetPunDemoBuildSettings();
+            bool doneBefore = EditorPrefs.GetBool("PunDemosOpenedBefore");
             EditorPrefs.SetBool("PunDemosOpenedBefore", true);
-            Debug.Log("No scene was open. Loaded PUN Demo Hub Scene and added demos to build settings. Ready to go! This auto-setup is now disabled in this Editor.");
-        }
-        else
-        {
             EditorApplication.update -= OnUpdate;
-        }
-    }
 
-    [MenuItem("Window/Photon Unity Networking/Configure Demos (build setup)", false, 5)]
-    public static void SetupDemo()
-    {
-        SetPunDemoBuildSettings();
-    }
-
-    //[MenuItem("Window/Photon Unity Networking/PUN Demo Loader Reset")]
-    //protected static void ResetDemoLoader()
-    //{
-    //    EditorPrefs.DeleteKey("PunDemosOpenedBefore");
-    //}
-
-    public static void LoadPunDemoHub()
-    {
-		string _hubScene = PhotonNetwork.FindAssetPath("DemoHub-Scene-V2 t:scene");
-		EditorSceneManager.OpenScene(_hubScene);
-		Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(_hubScene);
-    }
-
-
-
-
-    /// <summary>
-    /// Finds scenes in "Assets/Photon Unity Networking/Demos/", excludes those in folder "PUNGuide_M2H" and applies remaining scenes to build settings. The one with "Hub" in it first.
-    /// </summary>
-    public static void SetPunDemoBuildSettings()
-    {
-
-		string _PunPath = string.Empty;
-		
-		string _thisPath = PhotonNetwork.FindAssetPath ("PunStartUp");
-		
-		_thisPath = Application.dataPath + _thisPath.Substring (6); // remove "Assets/"
-
-		_PunPath = PhotonEditorUtils.GetParent(_thisPath,"Photon Unity Networking");
-
-		if (_PunPath == null)
-		{
-			_PunPath = Application.dataPath+"Photon Unity Networking";
-		}
-
-        // find path of pun guide
-		string[] tempPaths = Directory.GetDirectories(_PunPath, "Demos", SearchOption.AllDirectories);
-        if (tempPaths == null || tempPaths.Length != 1)
-        {
-            return;
-        }
-
-        // find scenes of guide
-        string guidePath = tempPaths[0];
-        tempPaths = Directory.GetFiles(guidePath, "*.unity", SearchOption.AllDirectories);
-
-        if (tempPaths == null || tempPaths.Length == 0)
-        {
-            return;
-        }
-
-        // add found guide scenes to build settings
-        List<EditorBuildSettingsScene> sceneAr = new List<EditorBuildSettingsScene>();
-        for (int i = 0; i < tempPaths.Length; i++)
-        {
-            //Debug.Log(tempPaths[i]);
-            string path = tempPaths[i].Substring(Application.dataPath.Length - "Assets".Length);
-            path = path.Replace('\\', '/');
-            //Debug.Log(path);
-
-            if (path.Contains("PUNGuide_M2H"))
+            if (doneBefore)
             {
-                continue;
+                return;
             }
 
-			// edited to avoid old scene to be included.
-			if (path.Contains("DemoHub-Scene-V2"))
+            if (string.IsNullOrEmpty(SceneManagerHelper.EditorActiveSceneName) && EditorBuildSettings.scenes.Length == 0)
             {
-                sceneAr.Insert(0, new EditorBuildSettingsScene(path, true));
-                continue;
+                LoadPunDemoHub();
+                SetPunDemoBuildSettings();
+                Debug.Log("No scene was open. Loaded PUN Demo Hub Scene and added demos to build settings. Ready to go! This auto-setup is now disabled in this Editor.");
             }
-
-            sceneAr.Add(new EditorBuildSettingsScene(path, true));
         }
 
-        EditorBuildSettings.scenes = sceneAr.ToArray();
-        EditorSceneManager.OpenScene(sceneAr[0].path);
+        [MenuItem("Window/Photon Unity Networking/Configure Demos (build setup)", false, 5)]
+        public static void SetupDemo()
+        {
+            SetPunDemoBuildSettings();
+        }
+
+        //[MenuItem("Window/Photon Unity Networking/PUN Demo Loader Reset")]
+        //protected static void ResetDemoLoader()
+        //{
+        //    EditorPrefs.DeleteKey("PunDemosOpenedBefore");
+        //}
+
+        public static void LoadPunDemoHub()
+        {
+            string scenePath = FindAssetPath("DemoHub-Scene t:scene");
+            if (!string.IsNullOrEmpty(scenePath))
+            {
+                    EditorSceneManager.OpenScene (scenePath);
+                    Selection.activeObject = AssetDatabase.LoadMainAssetAtPath (scenePath);
+            }
+        }
+
+        /// <summary>Finds the asset path base on its name or search query: https://docs.unity3d.com/ScriptReference/AssetDatabase.FindAssets.html </summary>
+        /// <returns>The asset path. String.Empty, if not found.</returns>
+        /// <param name="asset">Asset filter for AssetDatabase.FindAssets.</param>
+        public static string FindAssetPath(string asset)
+        {
+            string[] guids = AssetDatabase.FindAssets(asset, null);
+            if (guids.Length < 1)
+            {
+                Debug.LogError("We have a problem finding the asset: " + asset);
+                return string.Empty;
+            } else
+            {
+                return AssetDatabase.GUIDToAssetPath(guids[0]);
+            }
+        }
+
+        /// <summary>
+        /// Finds scenes in "Assets/Photon Unity Networking/Demos/", excludes those in folder "PUNGuide_M2H" and applies remaining scenes to build settings. The one with "Hub" in it first.
+        /// </summary>
+        public static void SetPunDemoBuildSettings()
+        {
+            string _PunPath = string.Empty;
+
+            string _thisPath = PhotonNetwork.FindAssetPath ("PunStartUp");
+
+            _thisPath = Application.dataPath + _thisPath.Substring (6); // remove "Assets/"
+
+            //_PunPath = PhotonEditorUtils.GetParent(_thisPath,"Photon");
+
+            if (string.IsNullOrEmpty(_PunPath))
+            {
+                _PunPath = Application.dataPath+"/Photon";
+            }
+
+            // find path of pun guide
+
+            string[] tempPaths = Directory.GetDirectories(_PunPath, "Demos*", SearchOption.AllDirectories);
+            if (tempPaths == null)
+            {
+                return;
+            }
+
+            List<EditorBuildSettingsScene> sceneAr = new List<EditorBuildSettingsScene> ();
+
+            // find scenes of guide
+            foreach (string guidePath in tempPaths)
+            {
+                tempPaths = Directory.GetFiles (guidePath, "*.unity", SearchOption.AllDirectories);
+
+                if (tempPaths == null || tempPaths.Length == 0)
+                {
+                    return;
+                }
+
+                // add found guide scenes to build settings
+                for (int i = 0; i < tempPaths.Length; i++)
+                {
+                    //Debug.Log(tempPaths[i]);
+                    string path = tempPaths [i].Substring (Application.dataPath.Length - "Assets".Length);
+                    path = path.Replace ('\\', '/');
+                    //Debug.Log(path);
+
+                    if (path.Contains ("PUNGuide_M2H") || path.Contains("DemoLoadBalancing"))
+                    {
+                        continue;
+                    }
+
+                    // edited to avoid old scene to be included.
+                    if (path.Contains ("DemoHub-Scene"))
+                    {
+                        sceneAr.Insert (0, new EditorBuildSettingsScene (path, true));
+                        continue;
+                    }
+
+                    sceneAr.Add (new EditorBuildSettingsScene (path, true));
+                }
+            }
+
+            EditorBuildSettings.scenes = sceneAr.ToArray();
+            EditorSceneManager.OpenScene(sceneAr[0].path);
+        }
     }
 }
--- Enlighter/Assets/Photon Unity Networking/Editor/PhotonNetwork/AccountService.cs
+++ Enlighter/Assets/Photon Unity Networking/Editor/PhotonNetwork/AccountService.cs
@@ -9,76 +9,101 @@
 // <author>developer@exitgames.com</author>
 // ----------------------------------------------------------------------------
 
+#if UNITY_2017_4_OR_NEWER
+#define SUPPORTED_UNITY
+#endif
+
 
 #if UNITY_EDITOR
-using System;
-using UnityEngine;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
-namespace ExitGames.Client.Photon
+namespace Photon.Realtime
 {
-   /// <summary>
+    using System;
+    using UnityEngine;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using ExitGames.Client.Photon;
+
+
+    /// <summary>
     /// Creates a instance of the Account Service to register Photon Cloud accounts.
     /// </summary>
     public class AccountService
     {
-        private const string ServiceUrl = "https://partner.photonengine.com/api/Unity/User/RegisterEx";
+        private const string ServiceUrl = "https://partner.photonengine.com/api/{0}/User/RegisterEx";
 
         private readonly Dictionary<string, string> RequestHeaders = new Dictionary<string, string>
         {
             { "Content-Type", "application/json" },
-            { "x-functions-key", "VQ920wVUieLHT9c3v1ZCbytaLXpXbktUztKb3iYLCdiRKjUagcl6eg==" }
+            { "x-functions-key", "" }
         };
 
+        private const string DefaultContext = "Unity";
+
+        private const string DefaultToken = "VQ920wVUieLHT9c3v1ZCbytaLXpXbktUztKb3iYLCdiRKjUagcl6eg==";
+
         /// <summary>
-        /// Attempts to create a Photon Cloud Account asynchronously.
-        /// Once your callback is called, check ReturnCode, Message and AppId to get the result of this attempt.
+        /// third parties custom context, if null, defaults to DefaultContext property value
         /// </summary>
+        public string CustomContext = null;     // "PartnerCode" on the server
+
+        /// <summary>
+        /// third parties custom token. If null, defaults to DefaultToken property value
+        /// </summary>
+        public string CustomToken = null;
+
+        /// <summary>
+        /// If this AccountService instance is currently waiting for a response. While pending, RegisterByEmail is blocked.
+        /// </summary>
+        public bool RequestPendingResult = false;
+
+        /// <summary>
+        /// Attempts to create a Photon Cloud Account asynchronously. Blocked while RequestPendingResult is true.
+        /// </summary>
+        /// <remarks>
+        /// Once your callback is called, check ReturnCode, Message and AppId to get the result of this attempt.
+        /// </remarks>
         /// <param name="email">Email of the account.</param>
         /// <param name="serviceTypes">Defines which type of Photon-service is being requested.</param>
         /// <param name="callback">Called when the result is available.</param>
-        public bool RegisterByEmail(string email, string serviceTypes, Action<AccountServiceResponse> callback = null, Action<string> errorCallback = null)
+        /// <param name="errorCallback">Called when the request failed.</param>
+        /// <param name="origin">Can be used to identify the origin of the registration (which package is being used).</param>
+        public bool RegisterByEmail(string email, List<ServiceTypes> serviceTypes, Action<AccountServiceResponse> callback = null, Action<string> errorCallback = null, string origin = null)
         {
+            if (this.RequestPendingResult)
+            {
+                Debug.LogError("Registration request pending result. Not sending another.");
+                return false;
+            }
+
             if (!IsValidEmail(email))
             {
                 Debug.LogErrorFormat("Email \"{0}\" is not valid", email);
                 return false;
             }
-            if (string.IsNullOrEmpty(serviceTypes))
+
+            string serviceTypeString = GetServiceTypesFromList(serviceTypes);
+            if (string.IsNullOrEmpty(serviceTypeString))
             {
                 Debug.LogError("serviceTypes string is null or empty");
                 return false;
             }
-            AccountServiceRequest req = new AccountServiceRequest();
-            req.Email = email;
-            req.ServiceTypes = serviceTypes;
-            return this.RegisterByEmail(req, callback, errorCallback);
-        }
 
-        public bool RegisterByEmail(string email, List<ServiceTypes> serviceTypes, Action<AccountServiceResponse> callback = null, Action<string> errorCallback = null)
-        {
-            if (serviceTypes == null || serviceTypes.Count == 0)
-            {
-                Debug.LogError("serviceTypes list is null or empty");
-                return false;
-            }
-            return this.RegisterByEmail(email, GetServiceTypesFromList(serviceTypes), callback, errorCallback);
-        }
+            string fullUrl = GetUrlWithQueryStringEscaped(email, serviceTypeString, origin);
 
-        public bool RegisterByEmail(AccountServiceRequest request, Action<AccountServiceResponse> callback = null, Action<string> errorCallback = null)
-        {
-            if (request == null)
-            {
-                Debug.LogError("Registration request is null");
-                return false;
-            }
+            RequestHeaders["x-functions-key"] = string.IsNullOrEmpty(CustomToken) ? DefaultToken : CustomToken;
+
+
+            this.RequestPendingResult = true;
+
             PhotonEditorUtils.StartCoroutine(
-                PhotonEditorUtils.HttpPost(GetUrlWithQueryStringEscaped(request),
+                PhotonEditorUtils.HttpPost(fullUrl,
                     RequestHeaders,
                     null,
                     s =>
                     {
+                        this.RequestPendingResult = false;
+                        //Debug.LogWarningFormat("received response {0}", s);
                         if (string.IsNullOrEmpty(s))
                         {
                             if (errorCallback != null)
@@ -104,6 +129,7 @@ namespace ExitGames.Client.Photon
                     },
                     e =>
                     {
+                        this.RequestPendingResult = false;
                         if (errorCallback != null)
                         {
                             errorCallback(e);
@@ -113,16 +139,15 @@ namespace ExitGames.Client.Photon
             return true;
         }
 
-        private static string GetUrlWithQueryStringEscaped(AccountServiceRequest request)
+
+        private string GetUrlWithQueryStringEscaped(string email, string serviceTypes, string originAv)
         {
-            #if UNITY_2017_3_OR_NEWER
-            string email = UnityEngine.Networking.UnityWebRequest.EscapeURL(request.Email);
-            string st = UnityEngine.Networking.UnityWebRequest.EscapeURL(request.ServiceTypes);
-            #else
-            string email = WWW.EscapeURL(request.Email);
-            string st = WWW.EscapeURL(request.ServiceTypes);
-            #endif
-            return string.Format("{0}?email={1}&st={2}", ServiceUrl, email, st);
+            string emailEscaped = UnityEngine.Networking.UnityWebRequest.EscapeURL(email);
+            string st = UnityEngine.Networking.UnityWebRequest.EscapeURL(serviceTypes);
+            string uv = UnityEngine.Networking.UnityWebRequest.EscapeURL(Application.unityVersion);
+            string serviceUrl = string.Format(ServiceUrl, string.IsNullOrEmpty(CustomContext) ? DefaultContext : CustomContext );
+
+            return string.Format("{0}?email={1}&st={2}&uv={3}&av={4}", serviceUrl, emailEscaped, st, uv, originAv);
         }
 
         /// <summary>
@@ -164,29 +189,32 @@ namespace ExitGames.Client.Photon
             }
         }
 
+        /// <summary>
+        /// Turns the list items to a comma separated string. Returns null if list is null or empty.
+        /// </summary>
+        /// <param name="appTypes">List of service types.</param>
+        /// <returns>Returns null if list is null or empty.</returns>
         private static string GetServiceTypesFromList(List<ServiceTypes> appTypes)
         {
-            if (appTypes != null)
+            if (appTypes == null || appTypes.Count <= 0)
             {
-                string serviceTypes = string.Empty;
-                if (appTypes.Count > 0)
-                {
-                    serviceTypes = ((int)appTypes[0]).ToString();
-                    for (int i = 1; i < appTypes.Count; i++)
-                    {
-                        int appType = (int)appTypes[i];
-                        serviceTypes = string.Format("{0},{1}", serviceTypes, appType);
-                    }
-                }
-                return serviceTypes;
+                return null;
             }
-            return null;
+
+            string serviceTypes = ((int)appTypes[0]).ToString();
+            for (int i = 1; i < appTypes.Count; i++)
+            {
+                int appType = (int)appTypes[i];
+                serviceTypes = string.Format("{0},{1}", serviceTypes, appType);
+            }
+
+            return serviceTypes;
         }
 
         // RFC2822 compliant matching 99.9% of all email addresses in actual use today
         // according to http://www.regular-expressions.info/email.html [22.02.2012]
         private static Regex reg = new Regex("^((?>[a-zA-Z\\d!#$%&'*+\\-/=?^_{|}~]+\\x20*|\"((?=[\\x01-\\x7f])[^\"\\]|\\[\\x01-\\x7f])*\"\\x20*)*(?<angle><))?((?!\\.)(?>\\.?[a-zA-Z\\d!#$%&'*+\\-/=?^_{|}~]+)+|\"((?=[\\x01-\\x7f])[^\"\\]|\\[\\x01-\\x7f])*\")@(((?!-)[a-zA-Z\\d\\-]+(?<!-)\\.)+[a-zA-Z]{2,}|\\[(((?(?<!\\[)\\.)(25[0-5]|2[0-4]\\d|[01]?\\d?\\d)){4}|[a-zA-Z\\d\\-]*[a-zA-Z\\d]:((?=[\\x01-\\x7f])[^\\\\[\\]]|\\[\\x01-\\x7f])+)\\])(?(angle)>)$",
-            RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
+             RegexOptions.CultureInvariant | RegexOptions.IgnoreCase);
         public static bool IsValidEmail(string mailAddress)
         {
             if (string.IsNullOrEmpty(mailAddress))
@@ -205,13 +233,7 @@ namespace ExitGames.Client.Photon
         public string Message;
         public Dictionary<string, string> ApplicationIds; // Unity's JsonUtility does not support deserializing Dictionary
     }
-    
-    [Serializable]
-    public class AccountServiceRequest
-    {
-        public string Email;
-        public string ServiceTypes;
-    }
+
 
     public class AccountServiceReturnCodes
     {
@@ -229,6 +251,8 @@ namespace ExitGames.Client.Photon
         TrueSync = 4,
         Pun = 5,
         Thunder = 6,
+        Quantum = 7,
+        Fusion = 8,
         Bolt = 20
     }
 }
