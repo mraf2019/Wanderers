a3a270911b9c217c2971e290206a564fc9837edf
Kikaze-K <2957535425@qq.com>
2023-08-02 21:28:03 +0800

    result page update

--- Enlighter/Assets/Scripts/ResultController.cs
+++ Enlighter/Assets/Scripts/ResultController.cs
@@ -6,9 +6,11 @@ public class ResultController : MonoBehaviour
 {
     // Start is called before the first frame update
     public TMPro.TMP_Text rank;
+    public TMPro.TMP_Text total;
     void Start()
     {
         rank.text = PlayerPrefs.GetInt("rank").ToString();
+        total.text = PlayerPrefs.GetInt("totalNum").ToString();
     }
 
     // Update is called once per frame
